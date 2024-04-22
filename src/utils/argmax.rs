62da7e8ac6475c2338304a28344622b5237439e3
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-04-22 22:31:14 +0200

    Update utils typing

--- src/utils/argmax.rs
+++ src/utils/argmax.rs
@@ -1,33 +1,48 @@
-pub fn argmax<T: common_traits::FiniteRangeNumber>(vec: &[T]) -> isize {
+use std::ops::Index;
+
+pub fn argmax<T: common_traits::FiniteRangeNumber, A: Index<usize, Output = T>>(vec: &A) -> isize
+where
+    for<'a> &'a A: IntoIterator,
+{
     let mut max = T::MIN;
     let mut argmax = -1;
-    for i in 0..vec.len() {
+    let mut i = 0;
+    for _ in vec {
         if vec[i] > max {
             argmax = i.try_into().unwrap();
             max = vec[i];
         }
+        i += 1;
     }
     argmax
 }
 
-pub fn filtered_argmax<T: common_traits::FiniteRangeNumber, N: common_traits::FiniteRangeNumber>(
-    vec: &[T],
-    tie_break: &[N],
-    filter: &[bool],
-) -> isize {
-    assert_eq!(vec.len(), tie_break.len());
-    assert_eq!(vec.len(), filter.len());
-
+pub fn filtered_argmax<
+    T: common_traits::FiniteRangeNumber,
+    N: common_traits::FiniteRangeNumber,
+    A: Index<usize, Output = T>,
+    V: Index<usize, Output = N>,
+    F: Index<usize, Output = bool>,
+>(
+    vec: &A,
+    tie_break: &V,
+    filter: &F,
+) -> isize
+where
+    for<'a> &'a A: IntoIterator,
+{
     let mut max = T::MIN;
     let mut max_tie_break = N::MIN;
     let mut argmax = -1;
+    let mut i = 0;
 
-    for i in 0..vec.len() {
+    for _ in vec {
         if filter[i] && (vec[i] > max || (vec[i] == max && tie_break[i] > max_tie_break)) {
             argmax = i.try_into().unwrap();
             max = vec[i];
             max_tie_break = tie_break[i];
         }
+        i += 1;
     }
 
     argmax
--- src/utils/argmin.rs
+++ src/utils/argmin.rs
@@ -1,33 +1,48 @@
-pub fn argmin<T: common_traits::FiniteRangeNumber>(vec: &[T]) -> isize {
+use std::ops::Index;
+
+pub fn argmin<T: common_traits::FiniteRangeNumber, A: Index<usize, Output = T>>(vec: &A) -> isize
+where
+    for<'a> &'a A: IntoIterator,
+{
     let mut min = T::MAX;
     let mut argmin = -1;
-    for i in 0..vec.len() {
+    let mut i = 0;
+    for _ in vec {
         if vec[i] < min {
             argmin = i.try_into().unwrap();
             min = vec[i];
         }
+        i += 1;
     }
     argmin
 }
 
-pub fn filtered_argmin<T: common_traits::FiniteRangeNumber, N: common_traits::FiniteRangeNumber>(
-    vec: &[T],
-    tie_break: &[N],
-    filter: &[bool],
-) -> isize {
-    assert_eq!(vec.len(), tie_break.len());
-    assert_eq!(vec.len(), filter.len());
-
+pub fn filtered_argmin<
+    T: common_traits::FiniteRangeNumber,
+    N: common_traits::FiniteRangeNumber,
+    A: Index<usize, Output = T>,
+    V: Index<usize, Output = N>,
+    F: Index<usize, Output = bool>,
+>(
+    vec: &A,
+    tie_break: &V,
+    filter: &F,
+) -> isize
+where
+    for<'a> &'a A: IntoIterator,
+{
     let mut min = T::MAX;
     let mut min_tie_break = N::MAX;
     let mut argmin = -1;
+    let mut i = 0;
 
-    for i in 0..vec.len() {
+    for _ in vec {
         if filter[i] && (vec[i] < min || (vec[i] == min && tie_break[i] < min_tie_break)) {
             argmin = i.try_into().unwrap();
             min = vec[i];
             min_tie_break = tie_break[i];
         }
+        i += 1;
     }
 
     argmin
