bfa5ce472baa4040f920c9d986f68bdd867ce4a2
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-04-30 03:44:04 +0200

    Add SumSweep to main

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use anyhow::Result;
 use dsi_progress_logger::{ProgressLog, ProgressLogger};
 use webgraph::graphs::BVGraph;
 use webgraph_algo::algo::bfv::*;
+use webgraph_algo::algo::diameter::*;
 use webgraph_algo::prelude::*;
 
 fn main() -> Result<()> {
     stderrlog::new().verbosity(2).init()?;
-    let graph =
-        BVGraph::with_basename(std::env::args().nth(1).expect("No graph basename provided"))
-            .load()?;
+    let basename = std::env::args().nth(1).expect("No graph basename provided");
+    let graph = BVGraph::with_basename(&basename).load()?;
+    let reversed_graph = BVGraph::with_basename(basename + "-t").load()?;
     let start = std::env::args()
         .nth(2)
         .unwrap_or("0".to_string())
@@ -43,6 +44,21 @@ fn main() -> Result<()> {
     let mut parallel_pl = ProgressLogger::default();
     parallel_pl.display_memory(true).local_speed(true);
     parallel_visit.visit(|_, _| {}, parallel_pl)?;
+
+    let mut sum_sweep = SumSweepDirectedDiameterRadius::new(
+        &graph,
+        &reversed_graph,
+        SumSweepOutputLevel::RadiusDiameter,
+        None,
+    )?;
+    let mut sum_sweep_pl = ProgressLogger::default();
+    sum_sweep_pl.display_memory(true).local_speed(true);
+    sum_sweep.compute(sum_sweep_pl)?;
+    main_pl.info(format_args!(
+        "Diameter: {:?}\tRadius: {:?}",
+        sum_sweep.diameter(),
+        sum_sweep.radius()
+    ));
 
     Ok(())
 }
