768f8d32ce373f479010437d8130f26f96a60aa8
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-09-25 00:10:10 +0200

    Add internal tests for HyperLogLogCounter

--- src/utils/hyper_log_log.rs
+++ src/utils/hyper_log_log.rs
@@ -1034,3 +1034,189 @@ where
         estimate
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_counter_creation() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .build::<usize>(10);
+
+        let counter_4 = counters.get_counter(4);
+
+        assert!(counter_4.cached_bits.is_none());
+        assert_eq!(counter_4.offset, 2.pow(6) * 4);
+    }
+
+    #[test]
+    fn test_counter_edit_inplace() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .with_word_type::<u64>()
+            .build(10);
+        let mut counter_4 = counters.get_counter(4);
+
+        counter_4.add(42);
+
+        assert_eq!(counters.residual_mask.count_ones(), 64);
+        assert!(counter_4.cached_bits.is_none());
+        for i in 0..10 {
+            let pointer = counters.bits.as_slice().as_ptr() as *const u64;
+            let slice = unsafe {
+                let ptr = pointer.add(i * counters.words_per_counter());
+                std::slice::from_raw_parts(ptr, counters.words_per_counter())
+            };
+            if i != 4 {
+                assert_eq!(slice, vec![0; counters.words_per_counter()]);
+            } else {
+                let mut ones = 0;
+                for word in slice {
+                    ones += word.count_ones();
+                }
+                assert!(ones > 0);
+            }
+        }
+    }
+
+    #[test]
+    fn test_counter_edit_cached() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .with_word_type::<u64>()
+            .build(10);
+        let mut counter_4 = counters.get_counter(4);
+
+        assert!(counter_4.cached_bits.is_none());
+
+        unsafe {
+            counter_4.cache();
+        }
+
+        assert!(counter_4.cached_bits.is_some());
+        assert_eq!(counter_4.cached_bits.as_ref().unwrap().1, false);
+        assert_eq!(
+            counter_4.cached_bits.as_ref().unwrap().0.as_slice(),
+            vec![0; counters.words_per_counter()]
+        );
+
+        counter_4.add(42);
+
+        assert_eq!(counter_4.cached_bits.as_ref().unwrap().1, true);
+        for i in 0..10 {
+            let pointer = counters.bits.as_slice().as_ptr() as *const u64;
+            let slice = unsafe {
+                let ptr = pointer.add(i * counters.words_per_counter());
+                std::slice::from_raw_parts(ptr, counters.words_per_counter())
+            };
+            assert_eq!(slice, vec![0; counters.words_per_counter()]);
+        }
+
+        let mut ones = 0;
+        for word in counter_4.cached_bits.as_ref().unwrap().0.as_slice() {
+            ones += word.count_ones();
+        }
+        assert!(ones > 0);
+    }
+
+    #[test]
+    fn test_counter_commit_changes() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .with_word_type::<u64>()
+            .build(10);
+        let mut counter_4 = counters.get_counter(4);
+        unsafe {
+            counter_4.cache();
+        }
+        counter_4.add(42);
+
+        unsafe {
+            counter_4.commit_changes(false);
+        }
+
+        assert!(counter_4.cached_bits.is_none());
+        for i in 0..10 {
+            let pointer = counters.bits.as_slice().as_ptr() as *const u64;
+            let slice = unsafe {
+                let ptr = pointer.add(i * counters.words_per_counter());
+                std::slice::from_raw_parts(ptr, counters.words_per_counter())
+            };
+            if i != 4 {
+                assert_eq!(slice, vec![0; counters.words_per_counter()]);
+            } else {
+                let mut ones = 0;
+                for word in slice {
+                    ones += word.count_ones();
+                }
+                assert!(ones > 0);
+            }
+        }
+    }
+
+    #[test]
+    fn test_counter_sync_changes() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .with_word_type::<u64>()
+            .build(10);
+        let mut counter_4 = counters.get_counter(4);
+        unsafe {
+            counter_4.cache();
+        }
+        counter_4.add(42);
+
+        unsafe {
+            counter_4.commit_changes(true);
+        }
+
+        assert!(counter_4.cached_bits.is_some());
+        assert_eq!(counter_4.cached_bits.as_ref().unwrap().1, false);
+        for i in 0..10 {
+            let pointer = counters.bits.as_slice().as_ptr() as *const u64;
+            let slice = unsafe {
+                let ptr = pointer.add(i * counters.words_per_counter());
+                std::slice::from_raw_parts(ptr, counters.words_per_counter())
+            };
+            if i != 4 {
+                assert_eq!(slice, vec![0; counters.words_per_counter()]);
+            } else {
+                assert_eq!(slice, counter_4.cached_bits.as_ref().unwrap().0.as_slice())
+            }
+        }
+    }
+
+    #[test]
+    fn test_counter_sync() {
+        let counters = HyperLogLogCounterArrayBuilder::new()
+            .with_log_2_num_registers(6)
+            .with_word_type::<u64>()
+            .build(10);
+        let mut counter_4 = counters.get_counter(4);
+        unsafe {
+            counter_4.cache();
+        }
+        counter_4.add(42);
+
+        unsafe {
+            counter_4.sync_to_backend();
+        }
+
+        assert!(counter_4.cached_bits.is_some());
+        assert_eq!(counter_4.cached_bits.as_ref().unwrap().1, false);
+        for i in 0..10 {
+            let pointer = counters.bits.as_slice().as_ptr() as *const u64;
+            let slice = unsafe {
+                let ptr = pointer.add(i * counters.words_per_counter());
+                std::slice::from_raw_parts(ptr, counters.words_per_counter())
+            };
+            if i != 4 {
+                assert_eq!(slice, vec![0; counters.words_per_counter()]);
+            } else {
+                assert_eq!(slice, counter_4.cached_bits.as_ref().unwrap().0.as_slice())
+            }
+        }
+    }
+}
