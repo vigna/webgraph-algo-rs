39b8ee09e7ace7a541c8e1953f188ef10fd65e7e
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-10-01 01:01:55 +0200

    Add kahan summation

---
+++ src/algo/hyperball/kahan_sum.rs
@@ -0,0 +1,35 @@
+pub struct KahanSummation {
+    /// The current value of the sum
+    value: f64,
+    /// The current correction
+    c: f64,
+}
+
+impl KahanSummation {
+    /// Creates a new Kahan summation instance
+    pub fn new() -> Self {
+        Self { value: 0.0, c: 0.0 }
+    }
+
+    /// Adds a value.
+    ///
+    /// # Arguments
+    /// - `v`: the value to add to the sum.
+    pub fn add(&mut self, v: f64) {
+        let y = v - self.c;
+        let t = self.value + y;
+        self.c = (t - self.value) - y;
+        self.value = t;
+    }
+
+    /// Returns the sum computed so far.
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+
+    /// Resets the current value and correction to 0.
+    pub fn reset(&mut self) {
+        self.value = 0.0;
+        self.c = 0.0;
+    }
+}
--- src/algo/hyperball/mod.rs
+++ src/algo/hyperball/mod.rs
@@ -1,3 +1,5 @@
 mod hyperball_impl;
 
 pub use hyperball_impl::HyperBall;
+
+mod kahan_sum;
