3ff7edd6755a86b672646bba660ac764d30fa6a0
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-10-02 17:09:31 +0200

    Chenge main to test hyperball

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
 use dsi_progress_logger::{ProgressLog, ProgressLogger};
-use std::path::Path;
-use webgraph::prelude::BvGraph;
-use webgraph_algo::algo::diameter::*;
+use epserde::deser::{Deserialize, Flags};
+use webgraph::prelude::{BvGraph, DCF};
+use webgraph::traits::SequentialLabeling;
+use webgraph_algo::algo::hyperball::*;
 use webgraph_algo::prelude::*;
+use webgraph_algo::utils::HyperLogLogCounterArrayBuilder;
 
 fn main() -> Result<()> {
     stderrlog::new()
@@ -12,7 +14,8 @@ fn main() -> Result<()> {
         .init()?;
     let basename = std::env::args().nth(1).expect("No graph basename provided");
     let graph = BvGraph::with_basename(&basename).load()?;
-    let reversed_graph = BvGraph::with_basename(basename + "-t").load()?;
+    let reversed_graph = BvGraph::with_basename(basename.clone() + "-t").load()?;
+    let cumulative = DCF::load_mmap(basename + ".dcf", Flags::empty())?;
     let main_pl = ProgressLogger::default();
     main_pl.info(format_args!("Starting test..."));
 
@@ -20,25 +23,21 @@ fn main() -> Result<()> {
     flags.set(MmapFlags::SHARED, true);
     flags.set(MmapFlags::RANDOM_ACCESS, true);
 
-    let mut sum_sweep_pl = ProgressLogger::default();
-    sum_sweep_pl
+    let mut hyper_ball_pl = ProgressLogger::default();
+    hyper_ball_pl
         .display_memory(true)
         .local_speed(true)
         .log_interval(std::time::Duration::from_secs(180));
-    let mut sum_sweep = SumSweepDirectedDiameterRadius::new(
-        &graph,
-        &reversed_graph,
-        SumSweepOutputLevel::RadiusDiameter,
-        None,
-        TempMmapOptions::CustomDir(Path::new("./graphs").to_owned(), flags),
-        sum_sweep_pl.clone(),
-    )?;
-    sum_sweep.compute(sum_sweep_pl)?;
-    main_pl.info(format_args!(
-        "Diameter: {:?}\tRadius: {:?}",
-        sum_sweep.diameter(),
-        sum_sweep.radius()
-    ));
+    let mut hyper_ball = HyperBallBuilder::new(&graph, cumulative.as_ref())
+        .with_transposed(Some(&reversed_graph))
+        .with_neighbourhood_function(true)
+        .with_hyperloglog_settings(
+            HyperLogLogCounterArrayBuilder::new()
+                .with_log_2_num_registers(6)
+                .with_num_elements_upper_bound(graph.num_nodes()),
+        )
+        .build(hyper_ball_pl.clone())?;
+    hyper_ball.run_until_done(hyper_ball_pl)?;
 
     Ok(())
 }
