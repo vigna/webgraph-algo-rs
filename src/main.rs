f5002e956391287b6ee081b98be0c2b43175bf80
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-10-22 18:04:49 +0200

    Prepare main for benchmark

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use dsi_progress_logger::{ProgressLog, ProgressLogger};
 use epserde::deser::{Deserialize, Flags};
 use webgraph::prelude::{BvGraph, DCF};
 use webgraph::traits::SequentialLabeling;
+use webgraph_algo::algo::diameter::*;
 use webgraph_algo::algo::hyperball::*;
+use webgraph_algo::algo::scc::*;
 use webgraph_algo::prelude::*;
 use webgraph_algo::utils::HyperLogLogCounterArrayBuilder;
 
@@ -12,7 +14,7 @@ fn main() -> Result<()> {
         .verbosity(2)
         .timestamp(stderrlog::Timestamp::Second)
         .init()?;
-    let basename = std::env::args().nth(1).expect("No graph basename provided");
+    let basename = std::env::args().nth(2).expect("No graph basename provided");
     let graph = BvGraph::with_basename(&basename).load()?;
     let reversed_graph = BvGraph::with_basename(basename.clone() + "-t").load()?;
     let cumulative = DCF::load_mmap(basename + ".dcf", Flags::empty())?;
@@ -23,20 +25,55 @@ fn main() -> Result<()> {
     flags.set(MmapFlags::SHARED, true);
     flags.set(MmapFlags::RANDOM_ACCESS, true);
 
-    let mut hyper_ball_pl = ProgressLogger::default();
-    hyper_ball_pl
-        .display_memory(true)
-        .local_speed(true)
-        .log_interval(std::time::Duration::from_secs(180));
-    let mut hyper_ball = HyperBallBuilder::new(&graph, cumulative.as_ref())
-        .transposed(Some(&reversed_graph))
-        .hyperloglog_settings(
-            HyperLogLogCounterArrayBuilder::new()
-                .log_2_num_registers(6)
-                .num_elements_upper_bound(graph.num_nodes()),
-        )
-        .build(hyper_ball_pl.clone())?;
-    hyper_ball.run_until_done(hyper_ball_pl)?;
+    let mem_options = TempMmapOptions::None;
+
+    match std::env::args()
+        .nth(1)
+        .expect("No operation provided")
+        .as_str()
+    {
+        "tarjan" => {
+            TarjanStronglyConnectedComponents::compute(&graph, false, mem_options, main_pl)?;
+        }
+        "diameter" => {
+            let threadpool = rayon::ThreadPoolBuilder::new()
+                .build()
+                .expect("Should be able to get global threadpool");
+
+            let mut diameter = SumSweepDirectedDiameterRadius::new(
+                &graph,
+                &reversed_graph,
+                SumSweepOutputLevel::RadiusDiameter,
+                &threadpool,
+                None,
+                mem_options,
+                main_pl.clone(),
+            )?;
+            diameter.compute(main_pl.clone())?;
+        }
+        "hyperball" => {
+            let log2m = std::env::args()
+                .nth(3)
+                .expect("No log2m value provided")
+                .parse()
+                .expect("Expected integer");
+
+            let mut hyperball = HyperBallBuilder::new(&graph, cumulative.as_ref())
+                .hyperloglog_settings(
+                    HyperLogLogCounterArrayBuilder::new()
+                        .log_2_num_registers(log2m)
+                        .mem_options(mem_options.clone())
+                        .num_elements_upper_bound(graph.num_nodes()),
+                )
+                .mem_settings(mem_options)
+                .sum_of_distances(true)
+                .sum_of_inverse_distances(true)
+                .transposed(Some(&reversed_graph))
+                .build(main_pl.clone())?;
+            hyperball.run_until_done(main_pl)?;
+        }
+        _ => panic!(),
+    }
 
     Ok(())
 }
