4fec17d298eb37284532a9463f8a3abac9a9171d
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-11-23 07:09:41 +0100

    Removed nonsensical method

--- src/utils/traits.rs
+++ src/utils/traits.rs
@@ -24,9 +24,6 @@ pub trait CounterLogic {
     /// Sets the contents of `self` to the contents of `other`.
     fn set_to(&self, dst: impl AsMut<Self::Backend>, src: impl AsRef<Self::Backend>);
 
-    /// The number of words of type `W` used by a counter.
-    fn words_per_counter(&self) -> usize;
-
     fn new_counter(&self) -> Self::Counter<'_>
     where
         Self: Sized;
